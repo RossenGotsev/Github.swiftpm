3048cc61d813601c66d5c22f6f47e1103ce32943
Rossen <Rgotsev6164@stu.d214.org>
2023-04-14 13:15:36 -0500

    padding

--- ContentView.swift
+++ ContentView.swift
@@ -8,27 +8,29 @@ struct ContentView: View {
     var body: some View {
         VStack {
             TextField("Integer 1", value: $Integer1, format: .number)
+                .padding()
+            
             TextField("Integer 2", value: $Integer2, format: .number)
-           
+                .padding()
             Text("\(answer)")
             
             Button("multiply"){
                 Integer1 * Integer2 == answer
             }
-            
+            .padding()
             Button("divide"){
                 
             }
-            
+            .padding()
             
             Button("add"){
                 
             }
-            
+            .padding()
             Button("subtract"){
                 
             }
-            
+            .padding()
         }
     }
 }
